61a61e92440b6b74aacc76c2779bb69319148667
researchase <ashwin.venkat@gmail.com>
2014-04-12 08:38:50 -0700

    my first version

---
+++ src/com/example/boston/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.boston;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.app.Activity;
+
+
+public class MainActivity extends Activity {
+	int counter;
+	Button add;
+	Button sub;
+	TextView display;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		counter = 0;
+		add = (Button) findViewById(R.id.bAdd);
+		sub = (Button) findViewById(R.id.bSub);
+		display =(TextView) findViewById(R.id.tvDisplay);
+		add.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				counter++;
+				display.setText("your total is "+ counter);
+			}
+		});
+		sub.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				counter--;
+				display.setText("your total is " + counter);
+				
+			}
+		});
+		
+	}
+
+
+}
---
+++ src/com/example/boston/Splash.java
@@ -0,0 +1,41 @@
+package com.example.boston;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+	MediaPlayer ourSong;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		ourSong = MediaPlayer.create(Splash.this, R.raw.bird);
+		ourSong.start();
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(2000);
+				} catch(InterruptedException e){
+					e.printStackTrace();
+				}finally{
+					Intent openStartingPoint = new Intent("com.example.boston.MENU");
+					startActivity(openStartingPoint);
+				}
+			}
+		};
+		timer.start();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		ourSong.release();
+		finish();
+	}
+
+}
